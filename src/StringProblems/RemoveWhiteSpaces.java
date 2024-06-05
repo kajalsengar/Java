76e12e2ff8663b22c9fbdfe1e3057f450ad92cb2
kajal sengar <sengarkajal089@gmail.com>
2024-06-05 21:54:02 +0530

    remove white spaces

---
+++ src/StringProblems/RemoveWhiteSpaces.java
@@ -0,0 +1,17 @@
+package StringProblems;
+
+import java.util.Scanner;
+
+public class RemoveWhiteSpaces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter any string..");
+        String string = sc.nextLine();
+        System.out.println("before removing white spaces= "+string);
+
+        String str = string.replaceAll("\\s","");
+        System.out.println("after removing white spaces= "+str);
+
+    }
+}
