44a01d60637077b94204586f82f270a83ac5b259
kajal sengar <sengarkajal089@gmail.com>
2024-06-08 22:14:40 +0530

    list

---
+++ src/CollectionFramwork/List/ArrayLists.java
@@ -0,0 +1,49 @@
+package CollectionFramwork.List;
+
+import java.util.ArrayList;
+
+public class ArrayLists {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        // add elements...
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+
+        //add in between...
+        list.add(0,0);
+        System.out.println(list);
+
+        // get element...
+      list.get(1);
+
+      //for printing...
+    int element = list.get(0);
+        System.out.println(element);
+
+        //set element...
+        list.set(0,4);
+        System.out.println(list);
+
+        //remove element...
+        list.remove(1);
+        System.out.println(list);
+
+        //check value present or not...
+        System.out.println(list.contains(2));
+
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+
+        list2.add(5);
+        list2.add(6);
+        list2.add(7);
+        System.out.println("list 2 = "+list2);
+
+        //to add all element of list2 in list
+        list.addAll(list2);
+        System.out.println(list);
+    }
+}
