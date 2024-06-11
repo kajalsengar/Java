5ab8f4e472ad9c7399455d415d4b77fe58fd0f61
kajal sengar <sengarkajal089@gmail.com>
2024-06-11 21:00:00 +0530

    hash set

---
+++ src/CollectionFramework/Set/HashSets.java
@@ -0,0 +1,28 @@
+package CollectionFramework.Set;
+
+import java.util.*;
+
+public class HashSets {
+    public static void main(String[] args) {
+        Set <Integer> set = new HashSet<>();
+        set.add(4);
+        set.add(2);
+        set.add(7);
+        System.out.println(set);
+
+        Set <Integer> set1= new HashSet<>();
+        set1.add(0);
+        set1.add(3);
+        //set1.add(2);
+        set1.add(5);
+        System.out.println(set1);
+
+        System.out.println(set.contains(2));
+
+        System.out.println(set.hashCode());
+
+        System.out.println(set.addAll(set1));
+        System.out.println(set.retainAll(set1));
+        System.out.println(set.containsAll(set1));
+    }
+}
