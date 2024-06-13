c0033c97ccbcd7fbddc68120b53a706fded03000
kajal sengar <sengarkajal089@gmail.com>
2024-06-13 22:50:10 +0530

    Hash map

--- src/CollectionFramework/Map/HashMaps.java
+++ src/CollectionFramework/Map/HashMaps.java
@@ -3,7 +3,7 @@ package CollectionFramework.Map;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Maps {
+public class HashMaps {
     public static void main(String[] args) {
         Map <Integer, Integer> map = new HashMap<>();
         map.put(3,3);
