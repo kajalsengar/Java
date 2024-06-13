b2112a15ed641f662f6d5993c66353fc88000b68
kajal sengar <sengarkajal089@gmail.com>
2024-06-13 22:03:12 +0530

    Tree Set

--- src/CollectionFramework/Set/TreeSets.java
+++ src/CollectionFramework/Set/TreeSets.java
@@ -8,14 +8,30 @@ public class TreeSets {
 
         treeSet.add("kajal");
         treeSet.add("upasana");
-        treeSet.add("raj");
         treeSet.add("parth");
+        treeSet.add("raj");
         System.out.println(treeSet);
 
         System.out.println(treeSet.first());
-        System.out.println(treeSet.pollFirst());
+        treeSet.pollFirst();
+        System.out.println(treeSet);
 
+        System.out.println(treeSet.higher(""));
+        System.out.println(treeSet.lower(""));
+        System.out.println(treeSet.ceiling(""));
+        System.out.println(treeSet);
 
+        TreeSet <String> treeSet1 = new TreeSet<String>();
+
+        treeSet1.add("Chenshi");
+        treeSet1.add("parth");
+        treeSet1.add("himanshi");
+        System.out.println(treeSet1);
+
+        treeSet.addAll(treeSet1);
+        System.out.println(treeSet);
+
+        treeSet.removeAll(treeSet1);
         System.out.println(treeSet);
     }
 }
