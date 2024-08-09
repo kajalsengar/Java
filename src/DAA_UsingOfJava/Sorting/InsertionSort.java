e94badd03f11119368460503e5358adee661b23e
kajal sengar <sengarkajal089@gmail.com>
2024-08-09 21:38:34 +0530

    Insertion sort

---
+++ src/DAA_UsingOfJava/Sorting/InsertionSort.java
@@ -0,0 +1,30 @@
+package DAA_UsingOfJava.Sorting;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int j;
+        int i;
+        int key;
+        int array []= {40,20,60,10,50,30};
+
+        System.out.println("Array before sorting....");
+        for (i=0; i<=array.length-1; i++){
+            System.out.print(array[i]+ " ");
+        }
+
+        for (j=1; j<array.length; j++) {
+            key=array[j];
+            i=j-1;
+            while (i>=0 && array[i]>key){
+                array[i+1]=array[i];
+                i=i-1;
+            }
+            array[i+1]=key;
+        }
+        System.out.println(" ");
+        System.out.println("Array after sorting....");
+        for (i=0; i<array.length; i++){
+            System.out.print(array[i]+ " ");
+        }
+    }
+}
