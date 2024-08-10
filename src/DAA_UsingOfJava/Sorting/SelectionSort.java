79f76b73ccee535289dfea59b494e8d47926428a
kajal sengar <sengarkajal089@gmail.com>
2024-08-10 21:10:57 +0530

    Selection sort

---
+++ src/DAA_UsingOfJava/Sorting/SelectionSort.java
@@ -0,0 +1,38 @@
+package DAA_UsingOfJava.Sorting;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int i;
+        int j;
+        int min;
+        int temp;
+
+        int array []= {20,10,50,60,40,30};
+        System.out.println("Array before sorting...");
+        for (i=0; i< array.length ; i++){
+            System.out.print(array[i]+ " ");
+        }
+
+        for (i=0; i< array.length; i++){
+            min=i;
+
+            for (j=i+1; j< array.length; j++){
+                if (array[j]<array[min]){
+                    min=j;
+                }
+            }
+
+            if(min!=i){
+                temp=array[min];
+                array[min]=array[i];
+                array[i]=temp;
+            }
+        }
+
+        System.out.println(" ");
+        System.out.println("Array after sorting ...");
+        for (i=0; i<= array.length-1; i++){
+            System.out.print(array[i]+ " ");
+        }
+    }
+}
