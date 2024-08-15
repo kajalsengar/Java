8a54b8addc1449c302e7b5335c4e4897f6a06de8
kajal sengar <sengarkajal089@gmail.com>
2024-08-15 20:28:55 +0530

    lenght

--- src/HackerRank/StringLength.java
+++ src/HackerRank/StringLength.java
@@ -1,4 +1,4 @@
-package basics.StringProblems;
+package HackerRank;
 
 import java.util.Scanner;
 
@@ -13,5 +13,16 @@ public class StringLength {
 
         sum= A.length()+B.length();
         System.out.println(sum);
+
+        if (A.compareTo(B) >0){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+
+        A= (A.substring(0,1)).toUpperCase()+A.substring(1);
+        B= (B.substring(0,1)).toUpperCase()+B.substring(1);
+
+        System.out.println(A+ " " +B);
     }
 }
