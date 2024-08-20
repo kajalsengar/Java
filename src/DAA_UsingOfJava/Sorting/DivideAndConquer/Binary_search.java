409aaf031186968ee660c6a69ece84cd315619b7
kajal sengar <sengarkajal089@gmail.com>
2024-08-20 20:26:18 +0530

    Binary search algo

--- src/DAA_UsingOfJava/Sorting/DivideAndConquer/Binary_search.java
+++ src/DAA_UsingOfJava/Sorting/DivideAndConquer/Binary_search.java
@@ -6,15 +6,15 @@ public class Binary_search {
     public static void main(String[] args) {
         int array []= {10,20,30,40,50,60,70};
         int mid = 0;
-        int first=0;
-        int last= array.length-1;
+        int first=array.length-1;
+        int last= 0;
         int f=0;
         System.out.println(array.length);
         System.out.println("Enter value of finding....");
         Scanner sc= new Scanner(System.in);
         int value= sc.nextInt();
 
-        while (first<=last){
+        while (last<=first){
             mid= (first+last)/2;
 
             if (array[mid]==value) {
